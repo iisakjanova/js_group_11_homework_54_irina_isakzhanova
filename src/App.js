bd88889a4879fbfda8c10186e8398bc0bfb4fa2b
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-07-26 16:07:09 +0600

    Moved main code from PlayingField to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import {useState} from "react";
+import {nanoid} from "nanoid";
 
 import PlayingField from "./components/PlayingField/PlayingField";
 import Counter from "./components/Counter";
 
-
 const App = () => {
+    const [cells, setCells] = useState(makeCells());
     const [tries, setTries] = useState(0);
+
+    function getRingIndex() {
+        return Math.floor(Math.random() * 36);
+    }
+
+    function makeCells() {
+        let cellsObj = {};
+        let ringIndex = getRingIndex();
+
+        for (let i = 0; i < 36; i++) {
+            const cellId = nanoid();
+            cellsObj[cellId] = {id: cellId, isOpen: false, hasItem: i === ringIndex}
+        }
+
+        return cellsObj;
+    }
+
+    const openCell = id => {
+        setCells({
+            ...cells,
+            [id]: {
+                ...cells[id],
+                isOpen: true
+            }
+        })
+    };
 
     const incrementTries = () => {
         setTries(tries + 1);
     };
 
+    const handleOnClickCell = id => {
+        if(cells[id].isOpen) {
+            return;
+        }
+
+        openCell(id);
+        incrementTries();
+    };
+
     return (
         <div className="App">
             <PlayingField
-                onCellClick={incrementTries}
+                cells={Object.values(cells)}
+                onCellClick={handleOnClickCell}
             />
             <Counter
                 tries={tries}
--- src/components/PlayingField/PlayingField.js
+++ src/components/PlayingField/PlayingField.js
@@ -1,55 +1,16 @@
-import React, {useState} from 'react';
-import {nanoid} from "nanoid";
-
+import React from 'react';
 import Cell from "../Cell/Cell";
 import './PlayingField.css';
 
 const PlayingField = props => {
-    const [cells, setCells] = useState(makeCells());
-
-    function getRingIndex() {
-        return Math.floor(Math.random() * 36);
-    }
-
-    function makeCells() {
-        let cellsObj = {};
-        let ringIndex = getRingIndex();
-
-        for (let i = 0; i < 36; i++) {
-            const cellId = nanoid();
-            cellsObj[cellId] = {id: cellId, isOpen: false, hasItem: i === ringIndex}
-        }
-
-        return cellsObj;
-    }
-
-    const openCell = id => {
-        setCells({
-            ...cells,
-            [id]: {
-                ...cells[id],
-                isOpen: true
-            }
-        })
-    };
-
-    const handleOnClickCell = id => {
-        if(cells[id].isOpen) {
-            return;
-        }
-
-        openCell(id);
-        props.onCellClick();
-    };
-
     return (
         <div className="playing-field">
-            {Object.values(cells).map(cell => (
+            {props.cells.map(cell => (
                 <Cell
                     key={cell.id}
                     isOpen={cell.isOpen}
                     hasItem={cell.hasItem}
-                    onClick={() => handleOnClickCell(cell.id)}
+                    onClick={() => props.onCellClick(cell.id)}
                 />
             ))}
         </div>
